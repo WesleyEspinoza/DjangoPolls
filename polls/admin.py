64dcb7fa5b8b9528e0082525b3fe4dba6e88507a
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2020-02-17 16:00:26 -0800

    part 4

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 """temp Doc String"""
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+from django.utils import timezone
+
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    """temp Doc String"""
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """temp Doc String"""
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,11 @@ class Question(models.Model):
 
     def was_published_recently(self):
         """temp Doc String"""
-        return self.pub_date >= timezone.now() - timezone.datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     """temp Doc String"""
