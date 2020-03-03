22562f94573fa3d4a16a5c02993d4fb575824c20
Erick Wesley Espinoza <haze@Ericks-MacBook-Pro.local>
2020-03-02 17:25:56 -0800

    route fix

--- DjangoPolls/urls.py
+++ DjangoPolls/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    url('/', include('polls.urls')),
+    url('', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
